104ea9cb117d11d7d7bed3c6d695f7bc0df9d6c1
Rizwan114 <rizwanbaloch0099@gmail.com>
2024-12-14 00:32:12 +0500

    edited some file

--- src/app/components/file-1/page.tsx
+++ src/app/components/file-1/page.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { log } from 'console';
+import React from 'react';
 
-function Page() {
+
+interface unkownPerson {
+  userId : number;
+  id : number;
+  title : string;
+  completed : boolean;
+}
+
+async function Page() {
+
+  const user = await fetch ("https://jsonplaceholder.typicode.com/todos")
+
+  const client : unkownPerson [] = await user.json();
+
+  console.log("client ==>" , client);
+  
   return (
     <div>
-      hello
+      {client.map((todo,map)=> (
+        <div className='border border-black gap-6 flex flex-col'>
+          <p>userId:T{todo.userId}</p>
+          <p>Id:{todo.id}</p>
+          <p>title : {todo.title}</p>
+          <p>completed :{`${todo.completed}`}</p>
+        </div>
+      ))}
     </div>
   )
 }
